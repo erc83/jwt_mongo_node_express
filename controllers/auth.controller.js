3f6d08c58710f65a08f28fa285b6e2643d57ad26
erc83 <espinoza.eric@gmail.com>
2024-09-02 00:13:44 -0400

    se implementa login de usuario y se valida registro de un solo email

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -37,13 +37,20 @@ export const register = async (req, res) => {
         const rol = await collectionRole.findOne({name: "user"});
         newUser.roles = [rol._id]
     }
-    
+
+
     try {
+
         const collectionUser = database.collection(collection_user);
+
+        const userFound = await collectionUser.findOne({ email: req.body.email });
+
+        if(userFound) return res.status(400).json({ message: "El correo ya tiene una cuenta creada"})
+
         const registerUser = await collectionUser.insertOne(newUser);
         const _id = registerUser.insertedId.toJSON()
         const token = jwt.sign({id: _id}, SECRET_KEY, {
-             expiresIn:600
+            expiresIn:600
         })
         res.status(200).json({ token })
     } catch (error) {
@@ -51,6 +58,28 @@ export const register = async (req, res) => {
     } finally{ 
         await client.close() 
     }
+}
+
+export const login = async ( req, res ) => {
+
+    const client = await getConnection();
+    const database = client.db(dbName);
+    const collectionUser = database.collection(collection_user);
+    
+    const userFound = await collectionUser.findOne({ email: req.body.email });
+
+    if(!userFound) return res.status(400).json({ message: "User not found"})
+
+    const matchPassword = await bcrypt.compare( req.body.password, userFound.password )
+
+    if(!matchPassword) return res.status(401).json({ token: null, message: "Invalid Password"})
+
+    const id = userFound._id.toJSON()
+    const token = jwt.sign({ id }, SECRET_KEY, {
+        expiresIn: 600
+    })
+
+    res.json({ token })
 }
 
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/auth.controller.js'
+import { register, login } from '../controllers/auth.controller.js'
 
 const router = express.Router();
+
+router.post('/login', login );
 
 router.post('/register', register);
 
